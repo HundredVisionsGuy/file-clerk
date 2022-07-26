16aedb7e4397351f9aa63bde0d864c0bda15d459
hundredvisionsguy <cwinikka@gmail.com>
2022-07-26 12:00:59 -0700

    cleaning up the pre-commit workflow

--- file_clerk/__init__.py
+++ file_clerk/__init__.py
@@ -1,9 +1,9 @@
-__version__ = '1.0.4'
 # __init__.py
-
 """Deal with files and file content.
 
 Modules exported by this package:
 - `clerk`: check for files, get file extensions, get file contents, and more.
 
 """
+
+__version__ = "1.0.5"
