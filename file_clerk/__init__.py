14838361c49eaa7604c409e10b1f6b9f1bb8dec1
Hundredvisionsguy <cwinikka@gmail.com>
2023-09-05 11:44:54 -0700

    fixing release yml for poetry install

--- file_clerk/__init__.py
+++ file_clerk/__init__.py
@@ -6,4 +6,4 @@ Modules exported by this package:
 
 """
 
-__version__ = "1.1.1"
+__version__ = "1.1.2"
--- tests/test_file_clerk.py
+++ tests/test_file_clerk.py
@@ -5,7 +5,7 @@ from file_clerk import clerk as clerk
 
 
 def test_version():
-    assert __version__ == "1.1.1"
+    assert __version__ == "1.1.2"
 
 
 # TODO - separate tests based on command-line flag
