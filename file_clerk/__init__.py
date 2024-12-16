a28d96a00ab07d7eaca95b8281e8f34b6894cb3c
hundredvisionsguy <winikkc@hsd.k12.or.us>
2024-12-16 14:20:24 -0800

    chore: updating versioning info

--- file_clerk/__init__.py
+++ file_clerk/__init__.py
@@ -6,4 +6,4 @@ Modules exported by this package:
 
 """
 
-__version__ = "1.1.8"
+__version__ = "1.1.9"
--- tests/test_file_clerk.py
+++ tests/test_file_clerk.py
@@ -5,7 +5,7 @@ from file_clerk import clerk as clerk
 
 
 def test_version():
-    assert __version__ == "1.1.8"
+    assert __version__ == "1.1.9"
 
 
 # TODO - separate tests based on command-line flag
