6d197b1222a9d1b94316bfcb30cdf5f983e7927f
Winikka <winikkc@hsd.k12.or.us>
2025-03-08 15:16:34 -0800

    chore: updating version...again

--- tests/__init__.py
+++
@@ -1,8 +0,0 @@
-# __init__.py
-"""A collection of functions for dealing with files and file content.
-
-### Module exported by this package:
-`clerk.py`: has functions to open files, determine file types, open project files
-of a type, write CSV files, etc.
-"""
-__version__ = "1.1.11"
